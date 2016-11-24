a59e4875ba23c028bf3812040758d69940b3139a
Rahul Verlekar <rahul.verlekar@bidchat.com>
2016-11-24 18:25:07 +0530

    COde refactoring done here

--- www/MakeAToast.js
+++ www/MakeAToast.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "MakeAToast", "coolMethod", [arg0]);
+exports.showToast = function(arg0, success, error) {
+    exec(success, error, "MakeAToast", "showToast", [arg0]);
 };
